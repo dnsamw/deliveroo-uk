61733880652245c467a8aed59d2b0637908067aa
dnsamw <dnsam.mit@gmail.com>
2024-07-21 17:04:16 +0530

    placeholder change

--- src/components/UI/IconSearchBar.tsx
+++ src/components/UI/IconSearchBar.tsx
@@ -8,7 +8,7 @@ function IconSearchBar({}: Props) {
     <div className='relative'>
     <span className='absolute inset-y-0 left-0 flex items-center pl-3'><IoSearchOutline className='text-2xl text-gray-400'/></span>
     <span className='absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer'><IoCloseOutline className='text-3xl text-gray-400'/></span>
-    <input className='border rounded-md text-2xl px-4 py-2 pl-12 w-full' type="text" placeholder="Tossed - St Martin's Lane" />
+    <input className='border rounded-md text-2xl px-4 py-2 pl-12 w-full' type="text" placeholder="Search Tossed - St Martin's Lane" />
     </div>
   )
 }
